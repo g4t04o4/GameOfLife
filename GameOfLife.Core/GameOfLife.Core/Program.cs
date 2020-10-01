8df7895df07ff293014dd13e45200c04794058a8
unknown <repmah@gmail.com>
2020-10-01 18:12:56 +0300

    actual code

---
+++ GameOfLife.Core/GameOfLife.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameOfLife.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
