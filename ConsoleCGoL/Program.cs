3d9db7350714f794e38aeecfbb37b3f5c2e03510
unknown <repmah@gmail.com>
2020-12-28 19:05:37 +0300

    Renamed variables

--- ConsoleCGoL/Program.cs
+++ ConsoleCGoL/Program.cs
@@ -3,51 +3,42 @@ using System.Threading;
 
 namespace ConsoleCGoL
 {
-    class Board
+    internal class Board
     {
-        private const int size = 10;
+        private const int Size = 10;
 
-        private bool[,] board = new bool[size, size];
-        private bool[,] lastGen = new bool[size, size];
+        private readonly bool[,] _board = new bool[Size, Size];
+        private bool[,] _lastGen = new bool[Size, Size];
 
         public void FillBoard()
         {
             var rand = new Random();
 
-            for (int i = 0; i < size; i++)
+            for (var i = 0; i < Size; i++)
             {
-                for (int j = 0; j < size; j++)
+                for (var j = 0; j < Size; j++)
                 {
                     if (rand.Next(0, 100) < 30)
                     {
-                        board[i, j] = true;
+                        _board[i, j] = true;
                     }
                     else
                     {
-                        board[i, j] = false;
+                        _board[i, j] = false;
                     }
                 }
             }
 
-            lastGen = board;
+            _lastGen = _board;
         }
 
         public void DrawBoard()
         {
-            for (int i = 0; i < size; i++)
+            for (var i = 0; i < Size; i++)
             {
-                for (int j = 0; j < size; j++)
+                for (var j = 0; j < Size; j++)
                 {
-                    if (board[i, j])
-                    {
-                        Console.Write(1);
-                    }
-                    else
-                    {
-                        Console.Write(0);
-                    }
-
-                    Console.Write(' ');
+                    Console.Write(_board[i, j] ? 'x' : ' ');
                 }
 
                 Console.WriteLine();
@@ -58,19 +49,19 @@ namespace ConsoleCGoL
 
         public void Advance()
         {
-            for (int i = 0; i < size; i++)
+            for (var i = 0; i < Size; i++)
             {
-                for (int j = 0; j < size; j++)
+                for (var j = 0; j < Size; j++)
                 {
-                    int lg = CountNeighbours(i, j);
-                    if (!board[i, j] && lg == 3)
+                    var lg = CountNeighbours(i, j);
+                    if (!_board[i, j] && lg == 3)
                     {
-                        board[i, j] = true;
+                        _board[i, j] = true;
                     }
 
-                    if (board[i, j] && (lg < 2 || lg > 3))
+                    if (_board[i, j] && (lg < 2 || lg > 3))
                     {
-                        board[i, j] = false;
+                        _board[i, j] = false;
                     }
                 }
             }
@@ -78,50 +69,50 @@ namespace ConsoleCGoL
 
         private int CountNeighbours(int i, int j)
         {
-            int counter = 0;
+            var counter = 0;
 
             if (i != 0)
             {
-                if (j != 0 && lastGen[i - 1, j - 1])
+                if (j != 0 && _lastGen[i - 1, j - 1])
                 {
                     counter++;
                 }
 
-                if (lastGen[i - 1, j])
+                if (_lastGen[i - 1, j])
                 {
                     counter++;
                 }
 
-                if (j != size-1 && lastGen[i - 1, j + 1])
+                if (j != Size - 1 && _lastGen[i - 1, j + 1])
                 {
                     counter++;
                 }
             }
 
-            if (i != size-1)
+            if (i != Size - 1)
             {
-                if (j != 0 && lastGen[i + 1, j - 1])
+                if (j != 0 && _lastGen[i + 1, j - 1])
                 {
                     counter++;
                 }
 
-                if (lastGen[i + 1, j])
+                if (_lastGen[i + 1, j])
                 {
                     counter++;
                 }
 
-                if (j != size-1 && lastGen[i + 1, j + 1])
+                if (j != Size - 1 && _lastGen[i + 1, j + 1])
                 {
                     counter++;
                 }
             }
 
-            if (j != 0 && lastGen[i, j - 1])
+            if (j != 0 && _lastGen[i, j - 1])
             {
                 counter++;
             }
 
-            if (j != size-1  && lastGen[i, j + 1])
+            if (j != Size - 1 && _lastGen[i, j + 1])
             {
                 counter++;
             }
@@ -130,19 +121,20 @@ namespace ConsoleCGoL
         }
     }
 
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Board board = new Board();
+            var board = new Board();
 
             board.FillBoard();
+            board.DrawBoard();
 
             while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Spacebar))
             {
                 board.Advance();
                 board.DrawBoard();
-                Thread.Sleep(500); 
+                Thread.Sleep(100);
             }
         }
     }
